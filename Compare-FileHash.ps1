ce834fafb5362a50e9e8502e872c137cc7c4a5e4
CoertK <43961752+CoertK@users.noreply.github.com>
2020-04-21 01:26:16 +0200

    Add files via upload

---
+++ Compare-FileHash.ps1
@@ -0,0 +1,24 @@
+﻿function Compare-FileHash([string]$folder1, [string]$folder2)
+{
+  $hashes1 = Get-ChildItem -Path "$folder1" -Recurse | Get-FileHash
+  $hashes2 = Get-ChildItem -Path "$folder2" -Recurse | Get-FileHash
+  $files = $hashes1.Path + $hashes2.Path
+  ""
+  "Identical files:"
+  foreach($h1 in $hashes1)
+  {
+    foreach($h2 in $hashes2)
+    {
+      if($h1.Hash -eq $h2.Hash)
+      {
+        $h1.Path + " == " + $h2.Path
+        $files = $files -ne $h1.Path
+        $files = $files -ne $h2.Path
+      }
+    }
+  }
+  ""
+  "Unique files:"
+  $files
+ ""
+}
